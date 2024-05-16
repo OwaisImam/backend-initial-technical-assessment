69190e75f997c2cf0a95c84989323bc8015188f1
Owais Imam <owais@get-licensed.co.uk>
2024-05-16 23:32:38 +0500

    Task 5

---
+++ app/Jobs/GenerateReportJob.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\GuestbookEntry;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+
+class GenerateReportJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+         try {
+            sleep(60);
+            $entries = GuestbookEntry::all();
+            Log::info(json_encode($entries));
+        } catch (\Exception $e) {
+            Log::error('Error generating report: ' . $e->getMessage());
+        }
+    }
+}
